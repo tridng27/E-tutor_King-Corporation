c97cf06bd67a62f41d1cc3721532acc08fcbd436
Phong <phphong2508@gmail.com>
2025-03-20 20:34:33 +0700

    social

--- Frontend/my-project/src/App.jsx
+++ Frontend/my-project/src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from "./pages/admin/dashboard";
 import { GlobalProvider } from "./context/GlobalContext";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Timetable from "./pages/admin/Timetable";
+import Social from "./pages/user/Social";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin-dashboard" element={<Dashboard />} />
         <Route path="/admin-timetable" element={<Timetable />} />
+        <Route path="/social" element={<Social />} />
       </Routes>
     </BrowserRouter>
     </GlobalProvider>
